9d8aac58594ee0404f5fe155d721033ee9333eb2
sorawot(kem) <sorawit.kam@gmail.com>
2025-07-07 16:37:52 +0700

    Updates

---
+++ configuration.js
@@ -0,0 +1,23 @@
+window.__CONST__ENV__ = {
+  "VITE_APP_NAME": "DocStorage",
+  "VITE_APP_VERSION": "1.0.6",
+  "VITE_APP_SINCE": "2023",
+  "VITE_APP_DESCRIPTION": "เก็บ / สแกน เอกสารต่าง ๆ",
+  "VITE_APP_CONTACT_URL": "https://www.siamsmile.co.th",
+  "VITE_VERSION_CHECKER_CONFIRM": "true",
+  "VITE_VERSION_CHECKER_CHECK_INTEVAL_MIN": "15",
+  "VITE_BASE_URL": "https://docstorage.siamsmile.co.th",
+  "VITE_API_URL": "https://apigw.siamsmile.co.th/docstorage",
+  "VITE_APIGW_BASEURL": "https://apigw.siamsmile.co.th",
+  "VITE_API_DOCUMENT_V2": "https://docapi.siamsmile.co.th/api",
+  "VITE_SSO_ISSUER": "https://oauthlogin.siamsmile.co.th",
+  "VITE_SSO_CLIENT_ID": "eaf56512-d1f9-4771-b2f9-cbc13d97edb4",
+  "VITE_SSO_SCOPE": "openid profile roles email employee_profile employee_team employee_position employee_department employee_branch docstorage_api sssdoc_api",
+  "VITE_DYNASOFT_KEY": "\"f0068aAAAAEtoiyYmg7GkcKkT8HkRgeRb/hOaxjcVOHwq9duMrlRRDHxghqNQWaLA6Yvzbq0f1snxYiKl/Z5dP0pJ3WGYR4s=\";",
+  "MODE": "production"
+};
+Object.freeze(window.__CONST__ENV__);
+Object.defineProperty(window, "__CONST__ENV__", {
+    configurable: false,
+    writable: false,
+});
