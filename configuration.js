c96e74270d205f267da2c55fbd97dbaa65bb7f26
sorawot(kem) <sorawit.kam@gmail.com>
2025-07-07 16:57:41 +0700

    Updates

--- configuration.js
+++ configuration.js
@@ -3,18 +3,18 @@ window.__CONST__ENV__ = {
   "VITE_APP_VERSION": "1.0.6",
   "VITE_APP_SINCE": "2023",
   "VITE_APP_DESCRIPTION": "เก็บ / สแกน เอกสารต่าง ๆ",
-  "VITE_APP_CONTACT_URL": "https://www.siamsmile.co.th",
+  "VITE_APP_CONTACT_URL": "https://github.com/sorawit-kamlangsub",
   "VITE_VERSION_CHECKER_CONFIRM": "true",
   "VITE_VERSION_CHECKER_CHECK_INTEVAL_MIN": "15",
-  "VITE_BASE_URL": "https://docstorage.siamsmile.co.th",
-  "VITE_API_URL": "https://apigw.siamsmile.co.th/docstorage",
-  "VITE_APIGW_BASEURL": "https://apigw.siamsmile.co.th",
-  "VITE_API_DOCUMENT_V2": "https://docapi.siamsmile.co.th/api",
-  "VITE_SSO_ISSUER": "https://oauthlogin.siamsmile.co.th",
-  "VITE_SSO_CLIENT_ID": "eaf56512-d1f9-4771-b2f9-cbc13d97edb4",
+  "VITE_BASE_URL": "https://docstorage.uatsiamsmile.com",
+  "VITE_API_URL": "https://apigw.uatsiamsmile.com/docstorage",
+  "VITE_APIGW_BASEURL": "https://apigw.uatsiamsmile.com",
+  "VITE_API_DOCUMENT_V2": "https://uatdocapi2.siamsmile.co.th/api",
+  "VITE_SSO_ISSUER": "https://authlogin.uatsiamsmile.com",
+  "VITE_SSO_CLIENT_ID": "3be2a15c-86a2-467d-8a0a-65b71d3427e3",
   "VITE_SSO_SCOPE": "openid profile roles email employee_profile employee_team employee_position employee_department employee_branch docstorage_api sssdoc_api",
-  "VITE_DYNASOFT_KEY": "\"f0068aAAAAEtoiyYmg7GkcKkT8HkRgeRb/hOaxjcVOHwq9duMrlRRDHxghqNQWaLA6Yvzbq0f1snxYiKl/Z5dP0pJ3WGYR4s=\";",
-  "MODE": "production"
+  "VITE_DYNASOFT_KEY": "\"t0193AgYAAHxGW+W7S0wKFJPAijiD3UFZ/FGKpZqnIKRLBf8P3ItlG6g1b5M8Q1VaBy6GdY/Dv95kSdLZNu4O8U42/ci3qaPG4Zpn8zjTqo7WqmgNVbSso9VVLdCnTz/9f5e2/vYoAu8E2HJAFoA5MIWzAT7tHB5LgEqAFQArxcAScJjFvv4CDsqKf+uPU6cqOO1+Z94p9zhZwalLztAgr9AUY8h2mADmJ2cFqATYIcD5Uts1CLaASoDtgF4yL++/5N4vaQ==\";",
+  "MODE": "uat"
 };
 Object.freeze(window.__CONST__ENV__);
 Object.defineProperty(window, "__CONST__ENV__", {
