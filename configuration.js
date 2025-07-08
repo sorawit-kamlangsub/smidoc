04c941bea10ec4763c5872ad56c47c908367df43
sorawit-kamlangsub <111478659+sorawit-kamlangsub@users.noreply.github.com>
2025-07-08 17:10:56 +0700

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -6,7 +6,7 @@ window.__CONST__ENV__ = {
   "VITE_APP_CONTACT_URL": "https://www.siamsmile.co.th",
   "VITE_VERSION_CHECKER_CONFIRM": "true",
   "VITE_VERSION_CHECKER_CHECK_INTEVAL_MIN": "15",
-  "VITE_BASE_URL": "http://localhost:3000",
+  "VITE_BASE_URL": "https://sorawit-kamlangsub.github.io/smidoc",
   "VITE_API_URL": "https://apigw.uatsiamsmile.com/docstorage",
   "VITE_APIGW_BASEURL": "https://apigw.devsiamsmile.com",
   "VITE_API_DOCUMENT_V2": "https://uatdocapi2.siamsmile.co.th/api",
